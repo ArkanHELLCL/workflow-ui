547454d3a5d931fc335f00e20e970cf609dd025d
ArkanHELLCL~ <lcastillop1975@hotmail.com.com>
2023-10-18 15:59:13 -0300

    componente botones

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -8,23 +8,34 @@ const { REQ_Adjuntos } = formulario;
 const { FOR_Botones } = formulario;
 const { FOR_Campos } = formulario;
 
-const Buttons = (grupos) => {
-    for (const [key, value] of Object.entries(grupos)){        
-        console.log(value)
-        const grps = value.map(grp => grp)
-        const botones = grps.map(btns => btns[0])        
-        return (
-            botones.map((btns) => 
-            <button key={btns.id} className='h-9 w-auto dark:bg-[#444444] border dark:border-[#666666] bordfer-[#b8b5b2] flex items-center pr-1 pl-2 border-r-0 last:border-r' title={btns.nombre}>
-                <ButtonIcon typeButton={btns.id} styles='w-5 h-5'strokeWidth='1.3'/>{
-                    btns.descripcion &&
-                    <span className='text-xs font-normal leading-tight w-fit px-2'>{btns.descripcion}</span>
-                }                                            
-            </button>
-            )
-        )
-        //console.log(value.map(btns => btns.map(btn => btn.id)))
-      }    
+const Buttons = () => {
+    let corr = 0;
+    let keygrp = '';
+    return(
+        <div className='flex items-center gap-3 pb-2'>
+        {
+            grupos.map(grp => {
+                corr = corr+1;
+                keygrp = 'btnGrp-' + corr;
+                return (
+                    <div key={keygrp} className='flex'>
+                    {
+                        grp.map(btns =>
+                            <button key={btns[0].id} className='h-9 w-auto dark:bg-[#444444] border dark:border-[#666666] bordfer-[#b8b5b2] flex items-center pr-1 pl-2 border-r-0 last:border-r' title={btns[0].nombre}>
+                                <ButtonIcon typeButton={btns[0].id} styles='w-5 h-5'strokeWidth='1.3'/>{
+                                    btns[0].descripcion &&
+                                    <span className='text-xs font-normal leading-tight w-fit px-2'>{btns[0].descripcion}</span>
+                                }                                            
+                            </button>
+                        )
+                    }
+                    </div>
+                )
+            })
+                
+        }
+        </div>
+    )
 }
 
 const grupos = FOR_Botones.map(grupo => grupo)
@@ -37,9 +48,7 @@ export function Formulario(){
     const fecha = (date) => {
         const newDate = new Date(date)
         return dias[newDate.getDay()] + ' ' + newDate.getDate() + '-' + (newDate.getMonth() + 1) + '-' + newDate.getFullYear() + ' ' + newDate.getHours() + ':' + newDate.getMinutes()        
-    }
-    let corr = 0;
-    let keygrp = '';
+    }    
     return(
         <>
         {request &&
@@ -51,29 +60,7 @@ export function Formulario(){
                             <h2 className='text-base font-light leading-tight'>Acción requerida: <strong className='text-green-600'>{request?.ESR_AccionFlujoDatos}</strong></h2>
                         </div>
                         <div className='grid text-right leading-tight absolute right-2 top-8'>
-                            <div className='flex items-center gap-3 pb-2'> 
-                            {                               
-                                grupos.map(grp => {
-                                    corr = corr+1;
-                                    keygrp = 'btnGrp-' + corr;
-                                    return (
-                                        <div key={keygrp} className='flex'>
-                                        {
-                                            grp.map(btns =>
-                                                <button key={btns[0].id} className='h-9 w-auto dark:bg-[#444444] border dark:border-[#666666] bordfer-[#b8b5b2] flex items-center pr-1 pl-2 border-r-0 last:border-r' title={btns[0].nombre}>
-                                                    <ButtonIcon typeButton={btns[0].id} styles='w-5 h-5'strokeWidth='1.3'/>{
-                                                        btns[0].descripcion &&
-                                                        <span className='text-xs font-normal leading-tight w-fit px-2'>{btns[0].descripcion}</span>
-                                                    }                                            
-                                                </button>
-                                            )
-                                        }
-                                        </div>
-                                    )
-                                })
-                                    
-                            }
-                            </div>
+                            <Buttons />
                             <span className='text-[11px] leading-tight'>{fecha(request?.DRE_FechaEdit)}</span>
                         </div>
                     </div>
