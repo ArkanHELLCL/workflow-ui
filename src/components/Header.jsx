28401cf228fd03d3554772b6c509f3dbf568db40
ArkanHELLCL~ <lcastillop1975@hotmail.com.com>
2023-10-26 11:34:05 -0300

    Compenetizado de los menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,6 +16,117 @@ import {
         TableIconPlus 
     } from "./icons.jsx";
 
+const ContentMenu = ({children, title}) => {
+    return (
+        <section className="flex content-start gap-3 shrink px-2 relative pb-5 pt-1 border border-l-0 border-t-0 border-b-0 border-[#5c5a59]">
+            {children}
+            <div className="absolute -bottom-1 leading-tight text-xs w-full text-center -left-1 items-center">
+                <span>{title}</span>
+            </div>
+        </section>
+    )
+}
+
+const IconMenu = ({children, title}) => {
+    return (
+        <div className="flex flex-col items-center gap-0 cursor-pointer hover:bg-[#e1dfdd] dark:hover:bg-[#484644] p-1">
+            <div className="h-11 w-11 flex items-center justify-center">
+                {children}
+            </div>
+            <div className="flex flex-col leading-tight text-xs items-center">
+                {title.map((item, index) => (
+                    <span key={index}>{item}</span>
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
+
+const CrearMenu = () => {
+    return (
+        <ContentMenu title={'Crear'}>
+            <IconMenu title={['Crear nuevo','requerimiento']}>                
+                <FlowPlusIcon styles='w-10 h-10' strokeWidth='2' />                                
+            </IconMenu>            
+        </ContentMenu>
+    )    
+}
+
+const Requerimiento = () => {
+    return (
+        <ContentMenu title={'Requerimiento'}>
+            <IconMenu title={['Generar','informe']}>
+                <GenReportIcon styles='w-8 h-8'/>
+            </IconMenu>
+            <IconMenu title={['Descargar','informe']}>
+                <DownReportIcon styles='w-8 h-8' />
+            </IconMenu>
+            <IconMenu title={['Mensaje']}>
+                <MessagesIcon styles='w-8 h-8'/>
+            </IconMenu>
+            <IconMenu title={['Pasos del','flujo']}>
+                <FlowStepIcon styles='w-10 h-10' />
+            </IconMenu>
+        </ContentMenu>
+    )    
+}
+
+const Acciones = () => {
+    return (
+        <ContentMenu title={'Acciones'}>
+            <IconMenu title={['Abrir']}>
+                <OpenFolderIcon styles='h-8 w-8'/>
+            </IconMenu>
+            <IconMenu title={['Impresión','rápida']}>
+                <PrinterIcon styles='h-11 w-11' strokeWidth="2"/>
+            </IconMenu>
+            <IconMenu title={['Quitar datos','adjuntos']}>
+                <DeleteFileIcon styles='text-red-500 h-10 w-10' strokeWidth={1} />
+            </IconMenu>            
+        </ContentMenu>
+    )    
+}
+
+const GuardarEquipo = () => {
+    return (
+        <ContentMenu title={'Guardar en el equipo'}>
+            <IconMenu title={['Guardar','como']}>
+                <SaveAsIconBig styles='h-7 w-7'/>
+            </IconMenu>
+            <IconMenu title={['Guardar todos los','datos adjuntos']}>
+                <SaveAllIconBig styles='h-8 w-8'/>
+            </IconMenu>            
+        </ContentMenu>
+    )    
+}
+
+const Mantenedores = () => {
+    return (
+        <ContentMenu title={'Mantenedor del sistema'}>
+            <IconMenu title={['Crear nuevo','registro']}>
+                <TableIconPlus styles='w-8 h-8' />
+            </IconMenu>
+            <IconMenu title={['Descargar','informe resultado']}>
+                <DownReportIcon styles='w-8 h-8' />
+            </IconMenu>            
+        </ContentMenu>
+    )    
+}
+
+const Informes = () => {
+    return (
+        <ContentMenu title={'Informe del sistema'}>
+            <IconMenu title={['Generar','informe']}>
+                <GenReportIcon styles='w-8 h-8'/>
+            </IconMenu>
+            <IconMenu title={['Descargar','informe resultado']}>
+                <DownReportIcon styles='w-8 h-8' />
+            </IconMenu>            
+        </ContentMenu>
+    )    
+}
+
 export default function Header(){
     const handleNotDragOver = (event) => {
         event.preventDefault();
@@ -25,161 +136,13 @@ export default function Header(){
     return (
         <header className='dark:bg-[#323130] bg-[#f3f2f1] flex items-center justify-start p-2 transition-color delay-75 h-fit drop-shadow-md drop dark:shadow-[#191919] shadow-[#d2d0ce] pl-14 relative dark:border-[#191919] border-[#d2d0ce] border-[3px] border-t-0 border-l-0 border-r-0 z-0 dark:text-gray-100 text-stone-500 fill-stone-500 dark:fill-stone-100'
         onDragOver={handleNotDragOver}>
-            <section className="flex content-start gap-3 shrink px-2 relative pb-5 pt-1 border border-l-0 border-t-0 border-b-0 border-[#5c5a59]">
-                <div className="flex flex-col items-center gap-0 cursor-pointer hover:bg-[#e1dfdd] dark:hover:bg-[#484644] p-1">
-                    <div className="h-11 w-11 flex items-center justify-center">
-                        <FlowPlusIcon styles='w-10 h-10' strokeWidth='2' />
-                    </div>
-                    <div className="flex flex-col leading-tight text-xs items-center">
-                        <span>Crear nuevo</span>
-                        <span>requerimiento</span>
-                    </div>
-                </div>
-                <div className="absolute -bottom-1 leading-tight text-xs w-full text-center -left-1 items-center">
-                    <span>Crear</span>
-                </div>
-            </section>
-            <section className="flex content-start gap-3 shrink px-2 relative pb-5 pt-1 border border-l-0 border-t-0 border-b-0 border-[#5c5a59]">
-                <div className="flex flex-col items-center gap-0 cursor-pointer hover:bg-[#e1dfdd] dark:hover:bg-[#484644] p-1">
-                    <div className="h-11 w-11 flex items-center justify-center">
-                        <GenReportIcon styles='w-8 h-8'/>
-                    </div>
-                    <div className="flex flex-col leading-tight text-xs items-center">
-                        <span>Generar</span>
-                        <span>informe</span>
-                    </div>
-                </div>
-                <div className="flex flex-col items-center gap-0 cursor-pointer hover:bg-[#e1dfdd] dark:hover:bg-[#484644] p-1">
-                    <div className="h-11 w-11 flex items-center justify-center">
-                        <DownReportIcon styles='w-8 h-8' />
-                    </div>
-                    <div className="flex flex-col leading-tight text-xs items-center">
-                        <span>Descargar</span>
-                        <span>informe</span>
-                    </div>
-                </div>
-                <div className="flex flex-col items-center gap-0 cursor-pointer hover:bg-[#e1dfdd] dark:hover:bg-[#484644] p-1">
-                    <div className="h-11 w-11 flex items-center justify-center">
-                        <MessagesIcon styles='w-8 h-8' strokeWidth={2}/>
-                    </div>
-                    <div className="flex flex-col leading-tight text-xs items-center">
-                        <span>Mensajes</span>                        
-                    </div>
-                </div>
-                <div className="flex flex-col items-center gap-0 cursor-pointer hover:bg-[#e1dfdd] dark:hover:bg-[#484644] p-1">
-                    <div className="h-11 w-11 flex items-center justify-center">
-                        <FlowStepIcon styles='w-10 h-10' />
-                    </div>
-                    <div className="flex flex-col leading-tight text-xs items-center">
-                        <span>Pasos del</span>
-                        <span>flujo</span>
-                    </div>
-                </div>
-                <div className="absolute -bottom-1 leading-tight text-xs w-full text-center items-center -left-1">
-                    <span>Requerimiento</span>
-                </div>
-            </section>
-            <section className="flex content-start gap-3 shrink px-2 relative pb-5 pt-1 border border-l-0 border-t-0 border-b-0 border-[#5c5a59]">
-                <div className="flex flex-col items-center gap-0 cursor-pointer hover:bg-[#e1dfdd] dark:hover:bg-[#484644] p-1">
-                    <div className="h-11 w-11 flex items-center justify-center">
-                        <OpenFolderIcon styles='h-8 w-8'/>
-                    </div>
-                    <div className="leading-tight text-xs items-center">
-                        <span>Abrir</span>
-                    </div>
-                </div>
-                <div className="flex flex-col items-center gap-0 cursor-pointer hover:bg-[#e1dfdd] dark:hover:bg-[#484644] p-1">
-                    <div className="h-11 w-11 flex items-center justify-center">
-                        <PrinterIcon styles='h-11 w-11' strokeWidth="2"/>
-                    </div>
-                    <div className="flex flex-col leading-tight text-xs items-center">
-                        <span>Impresión</span>
-                        <span>rápida</span>
-                    </div>
-                </div>
-                <div className="flex flex-col items-center gap-0 cursor-pointer hover:bg-[#e1dfdd] dark:hover:bg-[#484644] p-1">
-                    <div className="text-red-500 h-11 w-11 flex items-center justify-center">
-                        <DeleteFileIcon styles='h-10 w-10' strokeWidth={1} /> 
-                    </div>
-                    <div className="flex flex-col leading-tight text-xs items-center">
-                        <span>Quitar datos</span>
-                        <span>adjuntos</span>
-                    </div>
-                </div>
-                <div className="absolute -bottom-1 leading-tight text-xs w-full text-center -left-1 items-center">
-                    <span>Acciones</span>
-                </div>
-            </section>
-            <section className="flex content-start gap-3 shrink px-2 relative pb-5 pt-1 border border-l-0 border-t-0 border-b-0 border-[#5c5a59]">
-                <div className="flex flex-col items-center gap-0 cursor-pointer hover:bg-[#e1dfdd] dark:hover:bg-[#484644] p-1">
-                    <div className="h-11 w-11 flex items-center justify-center">
-                        <SaveAsIconBig styles='h-7 w-7'/>
-                    </div>
-                    <div className="flex flex-col leading-tight text-xs items-center">
-                        <span>Guardar</span>
-                        <span>como</span>
-                    </div>
-                </div>
-                <div className="flex flex-col items-center gap-0 cursor-pointer hover:bg-[#e1dfdd] dark:hover:bg-[#484644] p-1">
-                    <div className="h-11 w-11 flex items-center justify-center">
-                        <SaveAllIconBig styles='h-8 w-8'/>
-                    </div>
-                    <div className="flex flex-col leading-tight text-xs items-center">
-                        <span>Guardar todos los</span>
-                        <span>datos adjuntos</span>
-                    </div>
-                </div>                
-                <div className="absolute -bottom-1 leading-tight text-xs w-full text-center -left-1 items-center">
-                    <span>Guardar en el equipo</span>
-                </div>
-            </section>                   
-            <section className="flex content-start gap-3 shrink px-2 relative pb-5 pt-1 border border-l-0 border-t-0 border-b-0 border-[#5c5a59]">
-                <div className="flex flex-col items-center gap-0 cursor-pointer hover:bg-[#e1dfdd] dark:hover:bg-[#484644] p-1">
-                    <div className="h-11 w-11 flex items-center justify-center">
-                        <TableIconPlus styles='w-8 h-8' />
-                    </div>
-                    <div className="flex flex-col leading-tight text-xs items-center">
-                        <span>Crear nuevo</span>
-                        <span>registro</span>
-                    </div>
-                </div>
-                <div className="flex flex-col items-center gap-0 cursor-pointer hover:bg-[#e1dfdd] dark:hover:bg-[#484644] p-1">
-                    <div className="h-11 w-11 flex items-center justify-center">
-                        <DownReportIcon styles='w-8 h-8' />
-                    </div>
-                    <div className="flex flex-col leading-tight text-xs items-center">
-                        <span>Descargar</span>
-                        <span>informe resultado</span>
-                    </div>
-                </div>                
-                <div className="absolute -bottom-1 leading-tight text-xs w-full text-center -left-1 items-center">
-                    <span>Mantenedor del sistema</span>
-                </div>
-            </section>
-            <section className="flex content-start gap-3 shrink px-2 relative pb-5 pt-1 border border-l-0 border-t-0 border-b-0 border-[#5c5a59]">
-                <div className="flex flex-col items-center gap-0 cursor-pointer hover:bg-[#e1dfdd] dark:hover:bg-[#484644] p-1">
-                    <div className="h-11 w-11 flex items-center justify-center">
-                        <GenReportIcon styles='w-8 h-8'/>
-                    </div>
-                    <div className="flex flex-col leading-tight text-xs items-center">
-                        <span>Generar</span>
-                        <span>informe</span>
-                    </div>
-                </div>
-                <div className="flex flex-col items-center gap-0 cursor-pointer hover:bg-[#e1dfdd] dark:hover:bg-[#484644] p-1">
-                    <div className="h-11 w-11 flex items-center justify-center">
-                        <DownReportIcon styles='w-8 h-8' />
-                    </div>
-                    <div className="flex flex-col leading-tight text-xs items-center">
-                        <span>Descargar</span>
-                        <span>informe resultado</span>
-                    </div>
-                </div>                
-                <div className="absolute -bottom-1 leading-tight text-xs w-full text-center -left-1 items-center">
-                    <span>Informe del sistema</span>
-                </div>
-            </section>
             <Suspense fallback={<Loading />}>
+                <CrearMenu />
+                <Requerimiento />
+                <Acciones />
+                <GuardarEquipo />                 
+                <Mantenedores />
+                <Informes />            
                 <DarkModeToggle />            
             </Suspense>
         </header>
