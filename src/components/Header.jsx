9fabfad9f8ad34ed588689bc272f6f1cd0415847
ArkanHELLCL <lcastillop1975hotmail.com>
2023-10-25 21:44:15 -0300

    Iconos menu header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@
 import { Suspense } from "react";
 import { DarkModeToggle } from "./darkMode.jsx";
 import Loading from "./Loading.jsx";
-import { DeleteFileIcon, EnvelopePlusIcon, OpenFolderIcon, PrinterIcon, SaveAllIcon, SaveAllIconBig, SaveAsIcon, SaveAsIconBig } from "./icons.jsx";
+import { DeleteFileIcon, DownReportIcon, EnvelopePlusIcon, FlowPlusIcon, FlowStepIcon, GenReportIcon, MessagesIcon, OpenFolderIcon, PrinterIcon, SaveAllIcon, SaveAllIconBig, SaveAsIcon, SaveAsIconBig } from "./icons.jsx";
 
 export default function Header(){
     const handleNotDragOver = (event) => {
@@ -14,9 +14,9 @@ export default function Header(){
         <header className='dark:bg-[#323130] bg-[#f3f2f1] flex items-center justify-start p-2 transition-color delay-75 h-fit drop-shadow-md drop dark:shadow-[#191919] shadow-[#d2d0ce] pl-16 relative dark:border-[#191919] border-[#d2d0ce] border-[3px] border-t-0 border-l-0 border-r-0 z-0'
         onDragOver={handleNotDragOver}>
             <section className="flex content-start gap-3 shrink px-2 relative pb-5 pt-1 border border-l-0 border-t-0 border-b-0 border-[#5c5a59]">
-                <div className="flex flex-col items-center gap-0">
+                <div className="flex flex-col items-center gap-0 cursor-pointer">
                     <div className="h-11 w-11 flex items-center justify-center">
-                        <SaveAllIconBig />
+                        <FlowPlusIcon styles='w-9 h-9' strokeWidth='2' />
                     </div>
                     <div className="flex flex-col leading-tight text-xs items-center">
                         <span>Crear nuevo</span>
@@ -28,38 +28,39 @@ export default function Header(){
                 </div>
             </section>
             <section className="flex content-start gap-3 shrink px-2 relative pb-5 pt-1 border border-l-0 border-t-0 border-b-0 border-[#5c5a59]">
-                <div className="flex flex-col items-center gap-0">
+                <div className="flex flex-col items-center gap-0 cursor-pointer">
                     <div className="h-11 w-11 flex items-center justify-center">
-                        <SaveAllIconBig />
+                        <GenReportIcon styles='w-9 h-9'/>
                     </div>
                     <div className="flex flex-col leading-tight text-xs items-center">
                         <span>Generar</span>
                         <span>informe</span>
                     </div>
                 </div>
-                <div className="flex flex-col items-center gap-0">
+                <div className="flex flex-col items-center gap-0 cursor-pointer">
                     <div className="h-11 w-11 flex items-center justify-center">
-                        <SaveAllIconBig />
+                        <DownReportIcon styles='w-9 h-9' />
                     </div>
                     <div className="flex flex-col leading-tight text-xs items-center">
                         <span>Descargar</span>
                         <span>informe</span>
                     </div>
                 </div>
-                <div className="flex flex-col items-center gap-0">
+                <div className="flex flex-col items-center gap-0 cursor-pointer">
                     <div className="h-11 w-11 flex items-center justify-center">
-                        <SaveAllIconBig />
+                        <MessagesIcon styles='w-12 h-12' />
                     </div>
                     <div className="flex flex-col leading-tight text-xs items-center">
                         <span>Mensajes</span>                        
                     </div>
                 </div>
-                <div className="flex flex-col items-center gap-0">
+                <div className="flex flex-col items-center gap-0 cursor-pointer">
                     <div className="h-11 w-11 flex items-center justify-center">
-                        <SaveAllIconBig />
+                        <FlowStepIcon styles='w-12 h-12' />
                     </div>
                     <div className="flex flex-col leading-tight text-xs items-center">
-                        <span>Pasos</span>                        
+                        <span>Pasos del</span>
+                        <span>flujo</span>
                     </div>
                 </div>
                 <div className="absolute -bottom-1 leading-tight text-xs w-full text-center items-center -left-1">
@@ -67,7 +68,7 @@ export default function Header(){
                 </div>
             </section>
             <section className="flex content-start gap-3 shrink px-2 relative pb-5 pt-1 border border-l-0 border-t-0 border-b-0 border-[#5c5a59]">
-                <div className="flex flex-col items-center gap-0">
+                <div className="flex flex-col items-center gap-0 cursor-pointer">
                     <div className="h-11 w-11 flex items-center justify-center">
                         <OpenFolderIcon styles='h-8 w-8'/>
                     </div>
@@ -75,7 +76,7 @@ export default function Header(){
                         <span>Abrir</span>
                     </div>
                 </div>
-                <div className="flex flex-col items-center gap-0">
+                <div className="flex flex-col items-center gap-0 cursor-pointer">
                     <div className="h-11 w-11 flex items-center justify-center">
                         <PrinterIcon styles='h-11 w-11' strokeWidth="2"/>
                     </div>
@@ -84,7 +85,7 @@ export default function Header(){
                         <span>rápida</span>
                     </div>
                 </div>
-                <div className="flex flex-col gap-0 pr-2 items-center">
+                <div className="flex flex-col gap-0 pr-2 items-center cursor-pointer">
                     <div className="text-red-500 h-11 w-11 flex items-center justify-center">
                         <DeleteFileIcon styles='h-10 w-10' strokeWidth={1} /> 
                     </div>
@@ -98,7 +99,7 @@ export default function Header(){
                 </div>
             </section>
             <section className="flex content-start gap-3 shrink px-2 relative pb-5 pt-1 border border-l-0 border-t-0 border-b-0 border-[#5c5a59]">
-                <div className="flex flex-col items-center gap-0">
+                <div className="flex flex-col items-center gap-0 cursor-pointer">
                     <div className="h-11 w-11 flex items-center justify-center">
                         <SaveAsIconBig />
                     </div>
@@ -107,7 +108,7 @@ export default function Header(){
                         <span>como</span>
                     </div>
                 </div>
-                <div className="flex flex-col items-center gap-0">
+                <div className="flex flex-col items-center gap-0 cursor-pointer">
                     <div className="h-11 w-11 flex items-center justify-center">
                         <SaveAllIconBig />
                     </div>
@@ -121,7 +122,7 @@ export default function Header(){
                 </div>
             </section>                   
             <section className="flex content-start gap-3 shrink px-2 relative pb-5 pt-1 border border-l-0 border-t-0 border-b-0 border-[#5c5a59]">
-                <div className="flex flex-col items-center gap-0">
+                <div className="flex flex-col items-center gap-0 cursor-pointer">
                     <div className="h-11 w-11 flex items-center justify-center">
                         <SaveAsIconBig />
                     </div>
@@ -130,7 +131,7 @@ export default function Header(){
                         <span>registro</span>
                     </div>
                 </div>
-                <div className="flex flex-col items-center gap-0">
+                <div className="flex flex-col items-center gap-0 cursor-pointer">
                     <div className="h-11 w-11 flex items-center justify-center">
                         <SaveAllIconBig />
                     </div>
@@ -144,21 +145,21 @@ export default function Header(){
                 </div>
             </section>
             <section className="flex content-start gap-3 shrink px-2 relative pb-5 pt-1 border border-l-0 border-t-0 border-b-0 border-[#5c5a59]">
-                <div className="flex flex-col items-center gap-0">
+                <div className="flex flex-col items-center gap-0 cursor-pointer">
                     <div className="h-11 w-11 flex items-center justify-center">
-                        <SaveAsIconBig />
+                        <GenReportIcon styles='w-9 h-9'/>
                     </div>
                     <div className="flex flex-col leading-tight text-xs items-center">
                         <span>Generar</span>
                         <span>informe</span>
                     </div>
                 </div>
-                <div className="flex flex-col items-center gap-0">
+                <div className="flex flex-col items-center gap-0 cursor-pointer">
                     <div className="h-11 w-11 flex items-center justify-center">
-                        <SaveAllIconBig />
+                        <DownReportIcon styles='w-9 h-9' />
                     </div>
                     <div className="flex flex-col leading-tight text-xs items-center">
-                        <span>Descarcar</span>
+                        <span>Descargar</span>
                         <span>informe resultado</span>
                     </div>
                 </div>                
