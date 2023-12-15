c6cd13fe2dac1553761f33cffaf29c79a9b32e48
ArkanHELLCL~ <lcastillop1975@hotmail.com.com>
2023-12-15 07:44:40 -0300

    Fix button background color in Formulario component

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -45,7 +45,7 @@ const Buttons = ({grupos, idGroups}) => {
                     <animated.div key={keygrp} className='flex' style={buttonsAnimation} id={keygrp}>
                     {
                         grp[0].botones.map(btns =>
-                            <button key={btns.id} className='h-9 w-auto dark:bg-[#444444] outline outline-[1px] dark:outline-[#575757] outline-[#b8b5b2] hover:outline-[#0078d4] hover:dark:outline-[#b1b1b1] flex items-center pr-1 pl-2 hover:bg-[#eff6fc] dark:hover:bg-[#666666] z-10 hover:z-20' title={btns.nombre}>
+                            <button key={btns.id} className='h-9 w-auto dark:bg-[#444444] bg-white outline outline-[1px] dark:outline-[#575757] outline-[#b8b5b2] hover:outline-[#0078d4] hover:dark:outline-[#b1b1b1] flex items-center pr-1 pl-2 hover:bg-[#eff6fc] dark:hover:bg-[#666666] z-10 hover:z-20' title={btns.nombre}>
                                 <ButtonIcon typeButton={btns.id} styles='w-5 h-5'strokeWidth='1.3' typeIcon={1}/>{
                                     btns.nombre &&
                                     <span className='text-xs font-normal leading-tight w-fit px-2'>{btns.nombre}</span>
