bd7a801ef7a06ff06792c29b3cba84eaba5b60e1
ArkanHELLCL~ <lcastillop1975@hotmail.com.com>
2023-12-15 15:48:40 -0300

    Remove unused DropdownFilter component

--- src/components/InputTypes.jsx
+++ src/components/InputTypes.jsx
@@ -3,30 +3,6 @@ import { useState } from 'react';
 
 import { IconForm } from "./icons"
 
-const DropdownFilter = () => {
-    const [filter, setFilter] = useState('');
-  
-    const handleChangeFilter = event => {
-      setFilter(event.target.value);
-    }
-  
-    return (
-      <div>
-        <label htmlFor="filter">Filter: </label>
-        <select
-          name="filter"
-          value={filter}
-          onChange={handleChangeFilter}
-        >
-          <option value="">-- Please Select --</option>
-          <option value="name">Name</option>
-          <option value="date">Date</option>
-          <option value="category">Category</option>
-        </select>
-      </div>
-    )
-  };
-
 const InputType = ({campo}) => {    
     const classInput = "rounded-none rounded-e-lg bg-gray-50 border border-gray-300 dark:text-stone-100 text-stone-500 focus:ring-[#deecf9] focus:border-[#deecf9] block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-[#363636] dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-[#deecf9] dark:focus:border-[#deecf9]"    
 
@@ -40,7 +16,7 @@ const InputType = ({campo}) => {
         case 'T':
             return <textarea id={campo.FDI_NombreHTML} className={classInput} placeholder={campo.FDI_Descripcion} value={campo.DFO_Dato}/>
         case 'LM':
-            return <DropdownFilter />
+            return null
         default:
             return <input type="text" id={campo.FDI_NombreHTML} className={classInput} placeholder={campo.FDI_Descripcion} value={campo.DFO_Dato}/>
     }
