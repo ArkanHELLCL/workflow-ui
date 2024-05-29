c00064a98b4d8ed0cccd6acb462a8db2b03367a5
ArkanHELLCL~ <lcastillop1975@hotmail.com.com>
2024-05-29 16:43:10 -0400

    Eliminando comentarios

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -9,18 +9,10 @@ import { Spinner } from "./Spinner.jsx";
 import { EncontrarDescripcionPorId } from "./EncontrarDescripcionPorId.jsx";
 
 const MenuArbol = ({itemIdSelected, menuBandejas, mnuMantenedores, mnuReportes}) => {
-    /*let result = menuBandejas.find((item) => item === itemIdSelected)
-    console.log(itemIdSelected, menuBandejas,result)
-    if(!result) result = mnuMantenedores.find((item) => item === itemIdSelected)
-    if(!result) result = mnuReportes.find((item) => item === itemIdSelected)*/
-
     let url = ''
     itemIdSelected.charAt(0) === "b" ? url = EncontrarDescripcionPorId(itemIdSelected, menuBandejas[0]).url :
     itemIdSelected.charAt(0) === "m" ? url = EncontrarDescripcionPorId(itemIdSelected, mnuMantenedores[0]).url :
-    itemIdSelected.charAt(0) === "r" ? url = EncontrarDescripcionPorId(itemIdSelected, mnuReportes[0]).url : url = EncontrarDescripcionPorId('be', menuBandejas[0]).url 
-        
-    //const obj = menu.flujos.filter(item => parseInt(item.id) === filters.flujo)[0][tipoABuscar][0]
-    
+    itemIdSelected.charAt(0) === "r" ? url = EncontrarDescripcionPorId(itemIdSelected, mnuReportes[0]).url : url = EncontrarDescripcionPorId('be', menuBandejas[0]).url     
 
     window.history.pushState({},'',url)    
     
