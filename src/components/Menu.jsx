b789fed1423623313727372d694d3438d2eeb22e
ArkanHELLCL~ <lcastillop1975@hotmail.com.com>
2024-05-30 16:10:22 -0400

    del console.log

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -67,7 +67,6 @@ export default function Menu(){
                 ...filters,
                 itemIdSelected:idCurrentPath
             })
-            console.log(filters.itemIdSelected)
         }
     },[])
     
