401f2ceeecab5e96800caffd915b0f6b64af77af
ArkanHELLCL <lcastillop1975hotmail.com>
2024-06-03 20:19:20 -0400

    UserBar, menu component

--- src/components/HeaderBar.jsx
+++ src/components/HeaderBar.jsx
@@ -18,6 +18,8 @@ import CachedIcon from '@mui/icons-material/Cached';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
 import SearchIcon from '@mui/icons-material/Search';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import { ListItem } from '@mui/joy';
+import { Divider, ListItemButton } from '@mui/material';
 
 function SearchBar({openSearch, setOpenSearch, setFilters, filters}) {    
     const menuSearch = useId();
@@ -112,8 +114,7 @@ function SearchBar({openSearch, setOpenSearch, setFilters, filters}) {
                             </Menu>
                         </Dropdown>
                     </animated.div>
-                }
-                
+                }                
                 <form className="flex relative" onSubmit={HandleSubmit}>
                     <span className="absolute top-1 left-4 dark:text-[#ababab] text-sky-600">
                         <SearchIcon className="!w-5 !h-5 !-mt-1"/>
@@ -137,44 +138,48 @@ function SearchBar({openSearch, setOpenSearch, setFilters, filters}) {
     )
 }
 
-function UserBar({menuAppear, setOpen, open}) {    
-    const handleClickAway = () => {
-        setOpen(false);
-    };
+function UserBar({menuAppear, setOpen, open}) {
+    /*const handleClickAway = () => {
+        //setOpen(false);
+    };*/
     return(
-        <ClickAwayListener onClickAway={handleClickAway}>
-            <div className={`${open ? 'dark:bg-[#737373] dark:hover:bg-[#737373] bg-[#004578] hover:bg-[#004578]' : 'dark:hover:bg-[#363636] hover:bg-[#005a9e]'} p-3 h-full absolute right-5 flex items-center z-20 hover:cursor-pointer`} onClick={() => setOpen(!open)} title="Datos del perfil">                           
+        <Dropdown>
+            <MenuButton className={`${open ? 'dark:bg-[#737373] dark:hover:bg-[#737373] bg-[#004578] hover:bg-[#004578]' : 'dark:hover:bg-[#363636] hover:bg-[#005a9e]'} p-3 !h-full !absolute right-5 flex items-center z-20 hover:cursor-pointer !border-0 !min-h-fit !rounded-none !border-none`} title="Datos del perfil">
                 <span><img src={user.USR_Photo} className={`rounded-full w-[25px] h-[25px] hover:cursor-pointer`}/></span>
                 <span className="absolute inline-flex items-center justify-center w-2 h-2 text-xs font-bold text-white dark:bg-red-600 bg-red-500 rounded-full top-[1px] right-1"></span>
-                <animated.div style={menuAppear} className={`flex absolute z-[60] overflow-hidden right-0 top-[30px]`}>                
-                    <div className="h-full w-full dark:bg-[#262626] bg-[#ffffff] dark:border-[#737373] border-[#949494] border">
-                        <div className="w-full flex justify-end">
-                            <span className="flex border-[2px] dark:border-white border-black p-4 w-fit dark:text-white text-black text-xs hover:cursor-pointer dark:hover:bg-[#363636] hover:bg-[#d2d2d2]">Cerrar sesión</span>
-                        </div>
-                        <div className="flex justify-center items-center gap-5 h-fit p-4">
-                            <div className={`rounded-full w-[100px] h-[100px]`}>
-                                <img src={user.USR_Photo} className={`rounded-full h-full w-full`} />
-                            </div>
-                            <div className="flex flex-col h-fit max-w-[260px]">
-                                <span className="text-xl font-semibold truncate dark:text-white text-[#262626]">{user.USR_Nombre}</span>
-                                <span className="text-xs truncate pb-2 dark:text-white text-[#262626]">{user.USR_Mail}</span>
-                                <span className="text-xs truncate text-sky-600">{user.PER_Nombre}</span>
-                                <span className="text-xs truncate text-sky-600">{user.DEP_Descripcion}</span>
-                            </div>
-                        </div>
-                        <div className="w-full p-4 dark:bg-[#363636] dark:border-[#737373] hover:bg-[#d2d2d2] bg-[#f3f3f3] border-t border-[#d9d9d9] hover:dark:bg-[#505050] hover:cursor-pointer">
-                            <div className="flex items-center gap-3">
-                                <span className="relative text-[#313131] dark:text-white">
-                                    <MailOutlineIcon className="!w-10 !h-10" /> 
-                                    <span className="absolute inline-flex items-center justify-center w-4 h-4 text-xs font-bold text-white bg-red-600 rounded-full -top-2 -right-2">{user.USR_MsgSinLeer}</span>                               
-                                </span>                            
-                                <span className="text-[#313131] dark:text-white">Ver mis mensajes privados</span>
-                            </div>                        
-                        </div>
+            </MenuButton>
+            <Menu className='dark:!bg-[#262626] !bg-[#ffffff] dark:!border-[#737373] !border-[#949494] !border !rounded-none !h-auto !py-0'>                
+                <ListItem 
+                    endAction={
+                        <ListItemButton sx={{'border':'2px solid','paddingRight': '0px','marginRight':'-5px','marginTop':'10px'}} className="!border-[2px] !border-t-2 dark:!border-white !border-black !w-fit dark:!text-white !text-black !text-xs dark:hover:!bg-[#363636] hover:!bg-[#d2d2d2] !py-[13px] !px-6" onClick={()=>console.log("cerrar sesión")}>
+                            Cerrar Sesión
+                        </ListItemButton>}
+                    className=''
+                    >                    
+                </ListItem>
+                <div className="flex justify-center items-center gap-5 h-fit p-4">
+                    <div className={`rounded-full w-[100px] h-[100px]`}>
+                        <img src={user.USR_Photo} className={`rounded-full h-full w-full`} />
                     </div>
-                </animated.div>            
-            </div> 
-            </ClickAwayListener>      
+                    <div className="flex flex-col h-fit max-w-[260px]">
+                        <span className="text-xl font-semibold truncate dark:text-white text-[#262626]">{user.USR_Nombre}</span>
+                        <span className="text-xs truncate pb-2 dark:text-white text-[#262626]">{user.USR_Mail}</span>
+                        <span className="text-xs truncate text-sky-600">{user.PER_Nombre}</span>
+                        <span className="text-xs truncate text-sky-600">{user.DEP_Descripcion}</span>
+                    </div>
+                </div>
+            <Divider orientation="horizontal" />
+            <ListItem className="!px-16 !py-6 dark:!bg-[#363636] dark:!border-[#737373] !bg-[#f3f3f3] !border-t !border-[#d9d9d9] dark:!text-white !text-black" 
+                startAction={
+                    <ListItemButton className="!w-fit dark:!text-white !text-black !text-xs !pl-3 !pr-0 !px-0 !font-thin" 
+                        onClick={()=>console.log("ver mensajes")}>
+                            <MailOutlineIcon className="!w-10 !h-10" />
+                            <span className="absolute inline-flex items-center justify-center w-4 h-4 text-xs font-bold text-white bg-red-600 rounded-full top-0 -right-2">{user.USR_MsgSinLeer}</span>
+                    </ListItemButton>}>
+                Ver mis mensajes privados
+            </ListItem>
+            </Menu>
+        </Dropdown>
     )
 }
 
