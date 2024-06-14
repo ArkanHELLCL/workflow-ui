8f71238f5a3208d1f32eae67b87065632043bbb0
ArkanHELLCL <lcastillop1975hotmail.com>
2024-06-13 20:03:10 -0400

    Eliminacion de mensajes de error

--- src/components/headerbar/searchBar.jsx
+++ src/components/headerbar/searchBar.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/prop-types */
 import { useEffect, useId, useState } from "react";
 import { useSpring, animated } from "@react-spring/web";
