5a9adab2b975ab354f63f09dfe007d9524663c86
ArkanHELLCL <lcastillop1975@hotmail.com>
2024-06-18 17:22:54 -0400

    VAlidacion de menos a 1mm y superiores a 999mm RUT formato

--- src/components/main/formcontent/inputscomponents/FormInputRut.jsx
+++ src/components/main/formcontent/inputscomponents/FormInputRut.jsx
@@ -12,12 +12,22 @@ import { Fn } from '../../../../utils/validaRut.jsx';
 //import { forwardRef } from 'react';
 
 const formatearRut = (rutSinFormato) => {
-    //console.log('entro',rutSinFormato);
     const caracteresValidos = rutSinFormato.replace(/[^0-9kK]+/g, '');
-    const rutInvertido = caracteresValidos.split('').reverse().join('');
-    const rutFormateado = rutInvertido.replace(/^([kK\d])(\d{3})(\d{3})(\d{1,3})/,'$1-$2.$3.$4');
-    return rutFormateado.split('').reverse().join('')
-  };
+    const rutInvertido = caracteresValidos.slice(0,11).split('').reverse().join('');
+    const lenRut = rutInvertido.length
+    let rutFormateado
+    if(lenRut<7) return rutSinFormato
+    if(lenRut===7) {
+      rutFormateado = rutInvertido.replace(/^([kK\d])(\d{3})(\d{1,3})/,'$1-$2.$3');
+    }
+    if(lenRut>7 && lenRut<11) {
+      rutFormateado = rutInvertido.replace(/^([kK\d])(\d{3})(\d{3})(\d{1,3})/,'$1-$2.$3.$4');
+    }
+    if(lenRut>=11) {
+      rutFormateado = rutInvertido.replace(/^([kK\d])(\d{3})(\d{3})(\d{3})(\d{1,3})/,'$1-$2.$3.$4.$5');
+    }    
+    return rutFormateado?.split('').reverse().join('')
+};
 
 export const FormInputRut = ({ campo, className }) => {
   //const required = campo.FDI_CampoObligatorio === 1 ? {required : campo.FDI_ErrorMessage} : {required : false}
@@ -36,7 +46,7 @@ export const FormInputRut = ({ campo, className }) => {
             },
             maxLength: 13
           }}
-        defaultValue={campo.DFO_Dato}
+        defaultValue={formatearRut(campo.DFO_Dato)}
         render={({ field }) => (
             <FormControl
                 {...field}
