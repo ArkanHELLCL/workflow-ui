b84aacc2dd87d54b4820c0b85ec3ba40456f7414
ArkanHELLCL~ <lcastillop1975@hotmail.com.com>
2024-08-14 13:09:58 -0400

    Corrección texto mas informacion, regeneracion de mocks de bandejas

--- src/components/main/ListaRequerimientos.jsx
+++ src/components/main/ListaRequerimientos.jsx
@@ -7,26 +7,27 @@ import { Accordions } from './request/accordions.jsx'
 import { AccordionItem } from "./request/AccordioItem.jsx";
 import Loading from "../../utils/Loading.jsx";
 
-const Accordion = ({acc}) => {
+const Accordion = ({acc, moreItems}) => {    
     return(
         <>  
-            {acc.length===0 && (
+            {acc.length===0 && 
                 <div className="text-center flex justify-center lstRequestEmpty align-middle items-center h-full w-full !overflow-hidden">
                     <span className='text-[#2c87d2] text-xl w-full'>No se encontraron registros</span>
                 </div>            
-            )}
+            }
             {acc.map((item, index) => (
                 <Suspense key={index} fallback={<Loading />}>
-                    <AccordionItem key={index} item={item} />{
-                    item.requerimientos?.length === 1000 && 
-                        <div className="!text-xs font-semibold !text-center !text-stone-500 dark:!text-stone-100 flex flex-col py-3 gap-3">
-                            <span>Hay más elementos en esta carpeta en el servidor</span>
-                            <label htmlFor="btnMore" className="underline font-semibold text-xs text-[#2c87d2] cursor-pointer">Haga click aquí para ver más sobre elementos de esta carpeta</label>
-                            <button id="btnMore" className="hidden" onClick={()=>console.log("Ver más")}></button>
-                        </div>
-                    }
+                    <AccordionItem key={index} item={item} />
                 </Suspense>
-            ))}            
+            ))}{
+                moreItems ? 
+                    <div className="!text-xs font-semibold !text-center !text-stone-500 dark:!text-stone-100 flex flex-col py-3 gap-3">
+                        <span>Hay más elementos en esta carpeta en el servidor</span>
+                        <label htmlFor="btnMore" className="underline font-semibold text-xs text-[#2c87d2] cursor-pointer">Haga click aquí para ver más sobre elementos de esta carpeta</label>
+                        <button id="btnMore" className="hidden" onClick={()=>console.log("Ver más")}></button>
+                    </div>
+                    : null
+                }
         </>
     )
 }
@@ -38,13 +39,15 @@ export default function ListaRequerimientos(){
     const { requerimientoAccordion } = Accordions(filteredRequest, filters)
     const [acc, setAcc] = useState([])
 
+    const moreItems = filteredRequest.length === 1000 ? true : false
+    
     useEffect(() => {
         setAcc(requerimientoAccordion)
     }, [filters.flujo, filters.orderDes, filters.filter, filters.itemIdSelected, filters.stringSearch, filters.hoy, filters.filterSearch])
     
     return (        
         <Suspense fallback={<Loading />}>                           
-            <Accordion acc={acc}/>                     
+            <Accordion acc={acc} moreItems={moreItems}/>                     
         </Suspense>            
     )
 }
--- src/components/main/menu/MenuTree.jsx
+++ src/components/main/menu/MenuTree.jsx
@@ -1,8 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/prop-types */
 import { RichTreeView } from '@mui/x-tree-view/RichTreeView';
-import { TreeItem } from '@mui/x-tree-view/TreeItem';
-import { styled } from '@mui/material/styles';
 import { useFilters } from "../../../hooks/useFilters.jsx";
 import { useRequest } from '../../../hooks/useRequest.jsx';
 import { useRecords } from '../../../hooks/useRecords.jsx';
@@ -22,27 +20,7 @@ const getAllItemsWithChildrenItemIds = (menu) => {
 export function MenuTree({ menu, frmRecord }) { 
     const { filters, setFilters } = useFilters()
     const { setRecord } = useRecords()
-    const { setRequest } = useRequest()
-
-    const CustomTreeItem = styled(TreeItem)({   
-        '& .MuiTreeItem-content .MuiTreeItem-label' :{
-            fontSize:'13px',
-            fontWeight: 'lighter',
-            paddingLeft:'0px',
-            lineHeight: '1',
-            fontFamily: 'inherit'
-        },
-        '& .MuiTreeItem-content:hover' : {
-            backgroundColor: filters.darkMode ? 'rgba(56, 56, 56, 255)!important' : ''
-        },
-        '& .MuiTreeItem-content' : {
-            gap:'0px'
-        },
-        '& .MuiTreeItem-content.Mui-selected' : {
-            backgroundColor: 'rgb(25 118 210 / 26%)!important',
-            //color: 'rgba(56, 56, 56, 255)!important'
-        }
-    });
+    const { setRequest } = useRequest()    
 
     //const handleItemExpansionToggle = (event, itemId, isExpanded) => {
     const handleItemExpansionToggle = (event, itemId) => {
@@ -52,18 +30,33 @@ export function MenuTree({ menu, frmRecord }) {
             filterSearch: itemId === 'b' ? filters.filterSearch = 2 : filters.filterSearch = 1,
             stringSearch:'',
             filterSearchResult:false,
-            flujo : itemId==='bnc' ? 0 : filters.flujo
+            flujo : itemId.slice(0,2)==='bn' ? 0 : filters.flujo
         }))    
         setRequest(null)
         setRecord(null)
         frmRecord?.unregister()        
     };
-    return (
-        //menu.length > 0 &&
+    return (        
         <RichTreeView 
             items={menu} 
-            defaultExpandedItems={getAllItemsWithChildrenItemIds(menu)}
-            slots={{ item: CustomTreeItem }}
+            defaultExpandedItems={getAllItemsWithChildrenItemIds(menu)}            
+            sx={{'& .MuiTreeItem-content .MuiTreeItem-label' :{
+                    fontSize:'13px',
+                    fontWeight: 'lighter',
+                    paddingLeft:'0px',
+                    lineHeight: '1',
+                    fontFamily: 'inherit'
+                },
+                '& .MuiTreeItem-content:hover' : {
+                    backgroundColor: filters.darkMode ? 'rgba(56, 56, 56, 255)!important' : ''
+                },
+                '& .MuiTreeItem-content' : {
+                    gap:'0px'
+                },
+                '& .MuiTreeItem-content.Mui-selected' : {
+                    backgroundColor: 'rgb(25 118 210 / 26%)!important'                    
+                }
+            }}
             className={'dark:border-[#353535] px-1 border-[#d4d4d4] border-b-2 !pb-2 !mb-1 !text-stone-950 dark:!text-stone-100'}
             onItemExpansionToggle={handleItemExpansionToggle}
             onItemFocus={handleItemExpansionToggle}
