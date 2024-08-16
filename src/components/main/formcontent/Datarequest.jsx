83a7bfa2709e92c405c55bca1d1e6d45eed7bfec
ArkanHELLCL~ <lcastillop1975@hotmail.com.com>
2024-08-16 13:19:05 -0400

    Corrección de datos de cabecera del formaulario para cuando se llama a uno de la bandeja bnc

--- src/components/main/formcontent/Datarequest.jsx
+++ src/components/main/formcontent/Datarequest.jsx
@@ -6,8 +6,10 @@ export default function Datarequest() {
         <div id="dataRequest" className='w-full h-full grid'>
             <h1 className='text-base truncate w-auto pr-2'>{request?.request?.REQ_Descripcion.toUpperCase()}</h1>
             <h2 className='text-sm font-light leading-tight'>Flujo: <strong>{request?.request?.FLU_Descripcion}</strong> / Paso : <strong>{request?.request?.FLD_CodigoPaso}</strong></h2>
-            <h2 className='text-sm font-light leading-tight'>Para: <strong>{request?.request?.NombreEditor ? request?.request?.NombreEditor + ' ' + request?.request?.ApellidoEditor : request?.request?.DepDescripcionActual}</strong></h2>
-            <h2 className='text-sm font-light leading-tight'>Acción requerida: <strong className='text-green-600'>{request?.request?.NombreEditor ? request?.request?.ESR_AccionFlujoDatos : 'Tomar'}</strong></h2>   
+            <h2 className='text-sm font-light leading-tight'>Para: <strong>{request?.request?.NombreEditor ? request?.request?.NombreEditor + ' ' + request?.request?.ApellidoEditor : request?.request?.DepDescripcionActual}</strong></h2>{
+                request?.request?.Bandeja.slice(0,2) !== 'bn' &&            
+                    <h2 className='text-sm font-light leading-tight'>Acción requerida: <strong className='text-green-600'>{request?.request?.NombreEditor ? request?.request?.ESR_AccionFlujoDatos : 'Tomar'}</strong></h2>   
+            }
         </div>    
     );
 }
