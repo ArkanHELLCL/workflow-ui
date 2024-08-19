1036dfca4b183a5065d876d88221b641bc2acb7a
ArkanHELLCL <lcastillop1975hotmail.com>
2024-08-18 22:47:38 -0400

    Formaulario requerimiento responsive ver 1

--- src/components/main/formcontent/Inputs.jsx
+++ src/components/main/formcontent/Inputs.jsx
@@ -1,20 +1,16 @@
 /* eslint-disable react/prop-types */
 import InputsType from './InputsType.jsx'
 
-const colwidth = (campo) => {
-    return 'col-span-' + campo.FDI_TamanoDiseno
-}
-
 export default function Inputs({campos}){
     return(
-        <section id="InputsContent" className="py-3 w-full h-full flex">
-            <div className="w-full pr-2">
+        <section id="InputsContent" className="py-3 w-full h-full flex overflow-auto">
+            <div className="w-full pr-2 container">
                 <div className='grid grid-cols-12 gap-2'>{        
                     campos?.map((campo) => {
-                        //let colwidth = campo.FDI_TamanoDiseno === 12 ? 'col-span-12' : campo.FDI_TamanoDiseno === 11 ? 'col-span-11' : campo.FDI_TamanoDiseno === 10 ? 'col-span-10' : campo.FDI_TamanoDiseno === 9 ? 'col-span-9' : campo.FDI_TamanoDiseno === 8 ? 'col-span-8' : campo.FDI_TamanoDiseno === 7 ? 'col-span-7' : campo.FDI_TamanoDiseno === 6 ? 'col-span-6' : campo.FDI_TamanoDiseno === 5 ? 'col-span-5' : campo.FDI_TamanoDiseno === 4 ? 'col-span-4' : campo.FDI_TamanoDiseno === 3 ? 'col-span-3' : campo.FDI_TamanoDiseno === 2 ? 'col-span-2' : 'col-span-1'
+                        const colwidth = campo.FDI_TamanoDiseno === 12 ? 'col-span-12' : campo.FDI_TamanoDiseno === 11 ? '2xl:col-span-11 xl:col-span-12' : campo.FDI_TamanoDiseno === 10 ? '2xl:col-span-10 xl:col-span-12' : campo.FDI_TamanoDiseno === 9 ? '2xl:col-span-9 xl:col-span-12' : campo.FDI_TamanoDiseno === 8 ? '2xl:col-span-8 xl:col-span-12' : campo.FDI_TamanoDiseno === 7 ? '2xl:col-span-7 xl:col-span-12' : campo.FDI_TamanoDiseno === 6 ? '2xl:col-span-6 xl:col-span-12 md:col-span-12' : campo.FDI_TamanoDiseno === 5 ? 'sm:col-span-12 md:col-span-12 lg:col-span-12 xl:col-span-6 2xl:col-span-5' : campo.FDI_TamanoDiseno === 4 ? 'sm:col-span-12 md:col-span-12 lg:col-span-12 xl:col-span-6 2xl:col-span-4' : campo.FDI_TamanoDiseno === 3 ? 'sm:col-span-6 md:col-span-6 lg:col-span-6 xl:col-span-6 2xl:col-span-3' : campo.FDI_TamanoDiseno === 2 ? 'sm:col-span-6 md:col-span-6 lg:col-span-6 xl:col-span-6 2xl:col-span-2' : 'sm:col-span-6 md:col-span-6 lg:col-span-6 xl:col-span-6 2xl:col-span-1'
                         //colwidth = colwidth + ' relative'
                         return(
-                            <InputsType key={campo.FDI_NombreHTML} campo={campo} className={colwidth(campo) + ' relative pb-3'}/>                                
+                            <InputsType key={campo.FDI_NombreHTML} campo={campo} className={colwidth + ' relative pb-3'}/>                                
                         )}
                     )
                 }
