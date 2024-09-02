c22ad489b1b165cc8bcd4a16bd865f67ffac8b7d
ArkanHELLCL~ <lcastillop1975@hotmail.com.com>
2024-09-02 11:49:31 -0400

    Modificacion botones de formulario en el header para que trabajen sobre el formulario activo

--- src/components/header/formularioMenu.jsx
+++ src/components/header/formularioMenu.jsx
@@ -5,7 +5,6 @@ import ContentMenu from "./contentMenu.jsx"
 import { useRequest } from "../../hooks/useRequest.jsx";
 import { useFilters } from "../../hooks/useFilters.jsx";
 import { user } from "../../mocks/usuario.json";
-import Dropdown from '@mui/joy/Dropdown';
 import { ButtonIcon } from "../../utils/icons.jsx";
 
 const BtsFormulario = ({styles, keygrp, delay, grp, setAnimationEnd}) => {        
@@ -22,17 +21,19 @@ const BtsFormulario = ({styles, keygrp, delay, grp, setAnimationEnd}) => {
           }
           onEnter={() =>  setAnimationEnd(false)}>
             <div className={styles + ' flex-col h-full relative'} id={keygrp}>
-                <ContentMenu title={grp[0].descripcion}>{
+                <ContentMenu title={grp[0].descripcion} >{
                     grp[0].botones.map(btns =>
-                        <Dropdown key={btns.id}>
-                            <label htmlFor={btns.id}  className={`dark:hover:!bg-[#444444] hover:!bg-[#fefffe] !bg-transparent !rounded-none !m-0 !ps-2.5 !pe-2.5 dark:!text-stone-100 !text-stone-500 !font-thin !border-none !py-0 !my-0 !items-start cursor-pointer`} key={btns.id} title={btns.tooltiptext}>
+                        <button 
+                            key={btns.id} 
+                            className={`dark:hover:!bg-[#444444] hover:!bg-[#fefffe] !bg-transparent !rounded-none !m-0 !ps-2.5 !pe-2.5 dark:!text-stone-100 !text-stone-500 !font-thin !border-none !py-0 !my-0 !items-start cursor-pointer`} title={btns.tooltiptext} id={btns.id} 
+                            type={btns.type}
+                            form={'frmWorkFlowv4'}>
                                 <div className="flex flex-col leading-tight text-xs items-center relative text-nowrap h-full">
                                     <ButtonIcon typeButton={btns.id} styles='w-8 h-8'strokeWidth='1.3' typeIcon={2}/>
                                     <span className="!pt-2">{btns.descripcion[0]}</span>
                                     <span>{btns.descripcion[1]}</span>
                                 </div>
-                            </label>                
-                        </Dropdown>
+                        </button>
                     )}
                 </ContentMenu>
             </div>
