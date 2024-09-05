d89287b772a2bfc087730532236ad857dea46ff9
ArkanHELLCL~ <lcastillop1975@hotmail.com.com>
2024-09-05 15:41:05 -0400

    Diseño final para los calendarios en los parametros para los reportes

--- src/components/main/ReportFilters.jsx
+++ src/components/main/ReportFilters.jsx
@@ -1,8 +1,7 @@
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-//import { StaticDatePicker } from '@mui/x-date-pickers-pro';
-import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
 import { esES } from '@mui/x-date-pickers/locales';
 import 'dayjs/locale/es';
 import { useFilters } from '../../hooks/useFilters';
@@ -15,10 +14,108 @@ export default function ReportFilters() {
         () =>
             createTheme({
                 palette: {
-                mode: prefersDarkMode ? 'dark' : 'light',
+                    mode: prefersDarkMode ? 'dark' : 'light',
+                },
+                components: {
+                    MuiPickersLayout: {
+                        styleOverrides: {
+                          root: {                            
+                            minWidth: '220px !important',
+                            maxWidth: '220px !important',
+                            backgroundColor: 'transparent !important',
+                          }
+                        }
+                    },
+                    MuiPickersDay: {
+                      styleOverrides: {
+                        root: {
+                          color: prefersDarkMode ? '#bbdefb' : '#3883c1',
+                          borderRadius: '0px',
+                          borderWidth: '0px',
+                          border: '0px solid',
+                          backgroundColor: 'transparent',
+                          width: '25px',
+                          height: '25px',
+                          fontFamily: 'inherit'
+                        }
+                      }
+                    },
+                    MuiDayCalendar: {
+                        styleOverrides: {
+                          root: {
+                            color: prefersDarkMode ? '#bbdefb' : 'black',
+                            borderRadius: '0px',
+                            borderWidth: '0px',
+                            borderColor: '#2196f3',
+                            border: '0px solid',
+                            backgroundColor: 'transparent',
+                          }
+                        }
+                    },
+                    MuiDateCalendar: {
+                        styleOverrides: {
+                          root: {
+                            backgroundColor: 'transparent',
+                            width: '220px',
+                            height: '230px',
+                          }
+                        }
+                    },
+                    MuiPickersToolbar: {
+                        styleOverrides: {
+                          root: {
+                            color: prefersDarkMode ? '#bbdefb' : '#80c2f9',
+                            borderRadius: '0px',
+                            borderWidth: '0px',
+                            border: '0px solid',
+                            backgroundColor: 'transparent',
+                            padding: '5px 0px 10px 20px',
+                          }                          
+                        }
+                    },
+                    MuiTypography: {
+                        styleOverrides: {
+                            overline: {                                
+                                lineHeight: '.75rem',
+                                fontSize: '.65rem',
+                                color: '#bbdefb',
+                                fontFamily: 'inherit'
+                            },
+                            h4: {
+                                fontSize: '1.2rem',
+                                fontFamily: 'inherit'
+                            },
+                            caption:{
+                                width: '25px !important',
+                                height: '25px !important',
+                                fontFamily: 'inherit'
+                            }
+                        }
+                    },
+                    MuiPickersCalendarHeader: {
+                        styleOverrides: {
+                          root: {
+                            marginTop: '0px',
+                            paddingLeft: '20px',
+                            paddingRight: '0px',
+                          },
+                          label: {
+                            fontSize: '0.9rem',
+                            fontFamily: 'inherit'                            
+                          }
+                        }
+                    },
+                    MuiButtonBase: {
+                        styleOverrides: {
+                          root: {                            
+                            fontFamily: 'inherit',
+                            fontWeight: 100
+                          }
+                        }
+                    }
+                }
             },
-        },
-        esES),
+            esES),
         [prefersDarkMode],
     );
     
@@ -29,15 +126,32 @@ export default function ReportFilters() {
                 localeText={esES.components.MuiLocalizationProvider.defaultProps.localeText}
                 adapterLocale="es"
                 >
-                <div className='flex'>
-                
-                    <DateCalendar                        
-                        sx={
-                            {background : 'transparent'}
-                        }
-                        />
+                <div className='flex gap-0 dark:border-[#353535] border border-t-0 border-l-0 border-r-0'>
+                    <div className='grid'>
                         
+                        <StaticDatePicker
+                            localeText={{ toolbarTitle: "Seleccionar Fecha Desde:" }}
+                            slotProps={{
+                                actionBar: {
+                                    actions: ['today'],
+                                },
+                            }}
+                        />
+                    </div>
+                    <div className='grid'>
+                        
+                        <StaticDatePicker
+                            localeText={{ toolbarTitle: "Seleccionar Fecha Hasta:" }}
+                            slotProps={{
+                                actionBar: {
+                                    actions: ['today'],
+                                },
+                            }}
+                        />
+                    </div>
                 </div>
+                
+                
             </LocalizationProvider>
         </ThemeProvider>
     )
