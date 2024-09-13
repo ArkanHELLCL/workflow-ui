56eef2b254ebb7fcecd65dfea2f8afacfa904dd9
ArkanHELLCL <lcastillop1975hotmail.com>
2024-09-12 21:51:57 -0300

    Actualizacion de nombres de componentes

--- src/components/header/crearMenu.jsx
+++ src/components/header/crearMenu.jsx
@@ -80,36 +80,36 @@ export default function CrearMenu ({styles, delay, setAnimationEnd}) {
             onEnter={() =>  setAnimationEnd(false)}>   
                 <div className="h-full relative">
                     <ContentMenu title={'Crear'} styles={styles} className="flex-col h-full">
-                    <Button
-                        id="basic-button"
-                        aria-controls={open ? 'basic-menu' : undefined}
-                        aria-haspopup="true"
-                        aria-expanded={open ? 'true' : undefined}
-                        onClick={handleClick}
-                        className={`dark:hover:!bg-[#444444] hover:!bg-[#fefffe] !rounded-none dark:!text-stone-100 !text-stone-500 !font-thin !border-none !py-0 !my-0 !items-start !pt-1`} title="Crear un nuevo requerimiento"
-                    >
-                        <div className="flex flex-col items-center relative">
-                            <FlowPlusIcon styles='w-10 h-10' strokeWidth='2' />                         
-                            <span className='!leading-tight !text-xs !text-nowrap !font-thin !capitalize'>Crear nuevo</span>
-                            <span className='!leading-tight !text-xs !text-nowrap !font-thin !capitalize'>requerimiento</span>
-                            <KeyboardArrowDownIcon />
-                        </div>
-                    </Button>
-                    <Menu                        
-                        anchorEl={anchorEl}
-                        open={open}
-                        onClose={handleClose}
-                        MenuListProps={{
-                            'aria-labelledby': 'basic-button',
-                        }}                        
-                    >{
-                        flujos.filter(fls => fls.id>0).map((item) =>
-                            <MenuItem  className={`hover:!bg-[#c5c5c5] dark:hover:!bg-[#505050] !pr-10 dark:!text-stone-100 !text-stone-500 !py-0`}  key={item.id} onClick={() => hanldeOnClick(item)}>
-                                <ListItemIcon><FlowIcon id={item.id} /></ListItemIcon>
-                                <ListItemText>{item.description}</ListItemText>
-                            </MenuItem>
-                        )}
-                    </Menu>
+                      <Button
+                          id="create-menu"
+                          aria-controls={open ? 'create-menu' : undefined}
+                          aria-haspopup="true"
+                          aria-expanded={open ? 'true' : undefined}
+                          onClick={handleClick}
+                          className={`dark:hover:!bg-[#444444] hover:!bg-[#fefffe] !rounded-none dark:!text-stone-100 !text-stone-500 !font-thin !border-none !py-0 !my-0 !items-start !pt-1`} title="Crear un nuevo requerimiento"
+                      >
+                          <div className="flex flex-col items-center relative">
+                              <FlowPlusIcon styles='w-10 h-10' strokeWidth='2' />                         
+                              <span className='!leading-tight !text-xs !text-nowrap !font-thin !capitalize'>Crear nuevo</span>
+                              <span className='!leading-tight !text-xs !text-nowrap !font-thin !capitalize'>requerimiento</span>
+                              <KeyboardArrowDownIcon />
+                          </div>
+                      </Button>
+                      <Menu
+                          anchorEl={anchorEl}
+                          open={open}
+                          onClose={handleClose}
+                          MenuListProps={{
+                              'aria-labelledby': 'create-button',
+                          }}                        
+                      >{
+                          flujos.filter(fls => fls.id>0).map((item) =>
+                              <MenuItem  className={`hover:!bg-[#c5c5c5] dark:hover:!bg-[#505050] !pr-10 dark:!text-stone-100 !text-stone-500 !py-0`}  key={item.id} onClick={() => hanldeOnClick(item)}>
+                                  <ListItemIcon><FlowIcon id={item.id} /></ListItemIcon>
+                                  <ListItemText>{item.description}</ListItemText>
+                              </MenuItem>
+                          )}
+                      </Menu>
                     </ContentMenu>
                 </div>
             </Slide>  
