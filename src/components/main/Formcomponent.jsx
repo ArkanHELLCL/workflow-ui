e96cdade1f097d3d9e7780f40c931f3639e22ba8
ArkanHELLCL~ <lcastillop1975@hotmail.com.com>
2024-09-23 15:11:29 -0300

    Eliminacion del componente joy, correccion de estilos y refactor de componente header y formcomponent

--- src/components/main/Formcomponent.jsx
+++ src/components/main/Formcomponent.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import { useRequest } from '../../hooks/useRequest.jsx';
 import { usePreview } from '../../hooks/usePreview.jsx';
 import { useFilters } from '../../hooks/useFilters.jsx';
@@ -14,9 +14,23 @@ import {    NoData,
 import { formulario } from'../../mocks/formulario.json'
 import { formulario as formularioMant } from '../../mocks/formularioMant.json';
 import MPMant from './maintainer/proveedorMant.jsx'
-import Dropdown from '@mui/joy/Dropdown';
-import MenuButton from '@mui/joy/MenuButton';
+import Button from '@mui/material/Button';
 import TrendingFlatIcon from '@mui/icons-material/TrendingFlat';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { esES } from '@mui/x-data-grid/locales';
+
+function PreviewObj({preview, filters, request, frmRecord, campos}){
+    return(
+        preview.obj === 'X1' &&
+        <>
+            <div className="frmmanttitle">
+                <h2 className="font-thin text-xl">Mantenedor de proveedores</h2>
+                <h2 className="font-thin text-base border border-b border-t-0 border-r-0 border-l-0 dark:border-stone-700 pb-2">Ingreso unitario para formulario de {request?.request?.FLU_Descripcion}</h2>
+            </div>
+            <MPMant frmRecord={frmRecord} record={null} mant={filters.itemIdSelected} campos={campos} singleButton={true} />   
+        </>
+    )
+}
 
 export default function Formcomponent({frmRequest, frmRecord, filesList, setFilesList}){    
     const { request } = useRequest()
@@ -26,8 +40,40 @@ export default function Formcomponent({frmRequest, frmRecord, filesList, setFile
     const [dropEnter, setDropEnter] = useState(false);
     const [form, setForm] = useState()
     const [campos, setCampos] = useState([])
+    const prefersDarkMode = filters.darkMode
+    const theme = useMemo(
+      () =>
+        createTheme({
+          palette: {
+            mode: prefersDarkMode ? 'dark' : 'light',
+          },
+          typography: {
+            allVariants: {
+              fontFamily: "Segoe UI Web (West European) ,Segoe UI,-apple-system,BlinkMacSystemFont,Roboto,Helvetica Neue,sans-serif",
+              textTransform: 'none',
+              lineHeight: "1.5rem",
+              fontWeight: 400,
+              fontSize: '0.75rem'
+            },
+          },
+          components: {
+            MuiButton: {
+              defaultProps: {
+                palette: "palette"
+              },
+              styleOverrides: {
+                root: {
+                  padding: ".5rem 1.5rem"
+                }
+              }
+            }
+          }
+        },
+      esES),
+      [prefersDarkMode],
+    );
 
-    useEffect(() => {        
+    useEffect(() => {
         let frm
         if(request?.request?.VFO_Id){
             frm = formulario.filter(item => item.VFO_Id === request?.request?.VFO_Id && item.Bandeja === request?.request?.Bandeja)
@@ -55,35 +101,36 @@ export default function Formcomponent({frmRequest, frmRecord, filesList, setFile
         })        
     }
 
+    function PreviewButtonBack(){
+        return(
+            <Button variant="text" startIcon={<TrendingFlatIcon className="rotate-180" />} className={`hover:dark:!bg-[#505050] hover:!bg-[#e6f2fa] !border-0 dark:!text-stone-100 !text-stone-500 !text-xs !font-base !py-1 !rounded-none !ps-1 !pe-1 prevtitle w-fit ${preview.state && preview?.obj ? 'frmmantbackbuttom': '' }`} onClick={handleOnClick}>
+                Volver al formulario
+            </Button>
+        )
+    }
+
     return(
-        <>
+        <ThemeProvider theme={theme}>
             {
                 request && form &&
                 <section id="contentForm" className={`pl-4 pt-1 h-full w-full relative overflow-hidden flex flex-col z-50 columns-1${dropEnter ? ' dark:bg-[#1c1c1c]' : ''}`}>
                     <div className={`h-full w-full ${preview.state && preview?.selected!==null ? 'datapreview' : preview.state && preview.obj ? 'dataMantform' : 'dataform'} `}>
-                        <Header formulario={form} />
                         {
-                            !preview.state && !preview.obj &&
+                            !preview.state &&
                                 <>
+                                    <Header formulario={form} />
                                     <Files setFilesList={setFilesList} filesList={filesList}/>
                                     <Inputs dropEnter={dropEnter} setDropEnter={setDropEnter} campos={form.FOR_Campos} frmRequest={frmRequest} filesList={filesList} setFilesList={setFilesList}/>
                                 </>
                         }{
-                            preview.state && preview.obj === 'X1' &&
-                                <>
-                                    <Dropdown>
-                                        <MenuButton startDecorator={<TrendingFlatIcon className="rotate-180" />} className="hover:dark:!bg-[#505050] hover:!bg-[#e6f2fa] !border-0 dark:!text-stone-100 !text-stone-500 !text-xs !font-base !py-1 !rounded-none !ps-1 !pe-1 prevtitle w-fit frmmantbackbuttom" onClick={handleOnClick}>
-                                        Volver al formulario
-                                        </MenuButton>
-                                    </Dropdown>
-                                    <div className="frmmanttitle">
-                                        <h2 className="font-thin text-xl">Mantenedor de proveedores</h2>
-                                        <h2 className="font-thin text-base border border-b border-t-0 border-r-0 border-l-0 dark:border-stone-700 pb-2">Ingreso unitario para formulario de {request?.request?.FLU_Descripcion}</h2>
-                                    </div>
-                                    <MPMant frmRecord={frmRecord} record={null} mant={filters.itemIdSelected} campos={campos} singleButton={true} />   
-                                </>
+                            preview.state &&
+                                <PreviewButtonBack />
+                        }
+                        {
+                            preview.state && preview.obj &&
+                                <PreviewObj preview={preview} filters={filters} request={request} frmRecord={frmRecord} campos={campos}/>
                         }{
-                            preview.state && preview?.selected!==null &&
+                            preview.state && !preview.obj &&
                                 <Preview />
                         }
                     </div>
@@ -91,6 +138,6 @@ export default function Formcomponent({frmRequest, frmRecord, filesList, setFile
             }{  !form &&
                     <NoData />
             }
-        </>
+        </ThemeProvider>
     )
 }
--- src/components/main/formcontent/Header.jsx
+++ src/components/main/formcontent/Header.jsx
@@ -1,14 +1,9 @@
 /* eslint-disable react/prop-types */
-import { usePreview } from '../../../hooks/usePreview.jsx';
 import ReqTitle from './header/ReqTitle.jsx';
 import FrmHeader from './header/FrmHeader.jsx';
 import SenderData from './header/SenderData.jsx';
 import Buttons from "./header/ButtonsAction.jsx";
 import UpdateDate from './header/UpdateDate.jsx';
-
-import Dropdown from '@mui/joy/Dropdown';
-import MenuButton from '@mui/joy/MenuButton';
-import TrendingFlatIcon from '@mui/icons-material/TrendingFlat';
 
 const handleNotDragOver = (event) => {
     event.preventDefault();
@@ -16,33 +11,13 @@ const handleNotDragOver = (event) => {
     return false;
 }
 export default function Header({formulario}) {
-    const { preview, setPreview } = usePreview();
-    const handleOnClick = () => {
-        setPreview({
-            state:false,
-            obj:null,
-            selected:preview.selected
-        })        
-    }
-    
-    return (
-            !preview.state && !preview.obj ? (
-                <>                    
-                    <ReqTitle handleNotDragOver={handleNotDragOver}/>                   
-                    <FrmHeader handleNotDragOver={handleNotDragOver}/>
-                    <Buttons formulario={formulario} className={'frmbuttonsact'}/>
-                    <SenderData formulario={formulario} />
-                    <UpdateDate />
-                </>
-            ) :            
-            preview.state && (
-            <Dropdown>
-                <MenuButton startDecorator={<TrendingFlatIcon className="rotate-180" />} className="hover:dark:!bg-[#505050] hover:!bg-[#e6f2fa] !border-0 dark:!text-stone-100 !text-stone-500 !text-xs !font-base !py-1 !rounded-none !ps-1 !pe-1 prevtitle w-fit" onClick={handleOnClick}>
-                Volver al formulario
-                </MenuButton>
-            </Dropdown>
-            
-        )
-    
+    return (            
+        <>                    
+            <ReqTitle handleNotDragOver={handleNotDragOver}/>                   
+            <FrmHeader handleNotDragOver={handleNotDragOver}/>
+            <Buttons formulario={formulario} className={'frmbuttonsact'}/>
+            <SenderData formulario={formulario} />
+            <UpdateDate />
+        </>                
     )
 }
