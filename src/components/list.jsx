bbdaf47a30a1d6d51b43af84f00ad694ddb1df7b
ArkanHELLCL~ <lcastillop1975@hotmail.com.com>
2024-10-11 15:28:52 -0300

    Correccion de scroll para cuando no hay registros

--- src/components/list.jsx
+++ src/components/list.jsx
@@ -20,83 +20,83 @@ export default function List({frmRequest, frmRecord, frmReport}) {
     const { filters } = useFilters()
     const { inboxstate } = useInboxState()
     return (
-      <section id="list">
-        <div className='dark:text-stone-100 text-stone-500 dark:border-[#353535] border-[#d4d4d4] h-full grid border-r z-50 bg-[#ffffff] dark:bg-transparent pr-1 content-baseline'>
-              {
-                filters.itemIdSelected?.charAt(0).toUpperCase() === 'B' && filters.itemIdSelected.length > 1 &&
-                  <>
-                    <div className="pl-7 h-[30px] flex items-end justify-between leading-8 w-full z-40">
-                        <div className="flex gap-2 font-semibold z-50 transition-color delay-75">
-                          <Flujos />                
-                        </div>
-                        <div className="flex justify-end align-bottom pr-6 z-50 transition-color delay-75 h-full !pb-[2px]">
-                          <MenuFilters />
-                        </div>
-                    </div>
-                    <div className="overflow-auto h-full relative pr-2 w-full" id="containerRef">{
-                      (filters?.itemIdSelected === 'be' && !inboxstate.loadingBE) || (filters?.itemIdSelected === 'bs' && !inboxstate.loadingBS ) || (filters?.itemIdSelected === 'bf' && !inboxstate.loadingBF) || (filters?.itemIdSelected === 'bo' && !inboxstate.loadingBO) || (filters?.itemIdSelected === 'bnc' && !inboxstate.loadingBNC) || (filters?.itemIdSelected === 'bnw' && !inboxstate.loadingBNW) || (filters?.itemIdSelected === 'ba' && !inboxstate.loadingBA) ? 
-                        <div className='min-h-full overflow-hidden'>
-                            <Loading /> 
-                        </div>
-                      :
-                        <>
-                          <DetalleRequerimiento />
-                          <ListaRequerimientos frmRequest={frmRequest}/>
-                        </>
-                      }                        
-                    </div>
-                  </>
-              }{
-                filters.itemIdSelected?.charAt(0).toUpperCase() === 'M' && filters.itemIdSelected.length > 1 &&
-                  <>
-                    <div className="pl-7 h-[30px] flex items-end justify-between leading-8 w-full z-40 dark:border-[#353535] border-[#d4d4d4] border-b">{
-                      filters.itemIdSelected?.toUpperCase() === 'MU' ? (     
-                        <>                
-                          <div className="flex gap-2 font-semibold z-50 transition-color delay-75 h-full items-center">
-                                <Departamentos />                            
+      <section id="list">{
+        (filters?.itemIdSelected === 'be' && !inboxstate.loadingBE) || (filters?.itemIdSelected === 'bs' && !inboxstate.loadingBS ) || (filters?.itemIdSelected === 'bf' && !inboxstate.loadingBF) || (filters?.itemIdSelected === 'bo' && !inboxstate.loadingBO) || (filters?.itemIdSelected === 'bnc' && !inboxstate.loadingBNC) || (filters?.itemIdSelected === 'bnw' && !inboxstate.loadingBNW) || (filters?.itemIdSelected === 'ba' && !inboxstate.loadingBA) ? 
+          <div className='dark:text-stone-100 text-stone-500 dark:border-[#353535] border-[#d4d4d4] h-full grid border-r z-50 bg-[#ffffff] dark:bg-transparent pr-1 content-center'>
+              <Loading /> 
+          </div>
+        :
+          <div className='dark:text-stone-100 text-stone-500 dark:border-[#353535] border-[#d4d4d4] h-full grid border-r z-50 bg-[#ffffff] dark:bg-transparent pr-1 content-baseline'>
+                {
+                  filters.itemIdSelected?.charAt(0).toUpperCase() === 'B' && filters.itemIdSelected.length > 1 &&
+                    <>
+                      <div className="pl-7 h-[30px] flex items-end justify-between leading-8 w-full z-40">
+                          <div className="flex gap-2 font-semibold z-50 transition-color delay-75">
+                            <Flujos />                
                           </div>
                           <div className="flex justify-end align-bottom pr-6 z-50 transition-color delay-75 h-full !pb-[2px]">
-                                <MenuFiltersUsr />                            
+                            <MenuFilters />
                           </div>
-                        </> 
-                        ) : filters.itemIdSelected?.toUpperCase() === 'MC' ? (  
-                          <>                
-                            <div className="flex gap-2 font-semibold z-50 transition-color delay-75 h-full items-center">
-                                  <Regiones />                            
-                            </div>
-                            <div className="flex justify-end align-bottom pr-6 z-50 transition-color delay-75 h-full !pb-[2px]">
-                                  <MenuFiltersCom />                            
-                            </div>
-                          </> 
-                        ) : filters.itemIdSelected?.toUpperCase() === 'MI' ? (  
-                          <>                
-                            <div className="flex gap-2 font-semibold z-50 transition-color delay-75 h-full items-center">
-                                  <ListaDesplegable />                            
-                            </div>
-                            <div className="flex justify-end align-bottom pr-6 z-50 transition-color delay-75 h-full !pb-[2px]">
-                                  <MenuFiltersListaDes />                            
-                            </div>
-                          </> 
-                        ) : (
+                      </div>
+                      <div className="overflow-auto h-full relative pr-2 w-full" id="containerRef">
                           <>
+                            <DetalleRequerimiento />
+                            <ListaRequerimientos frmRequest={frmRequest}/>
+                          </>                      
+                      </div>
+                    </>
+                }{
+                  filters.itemIdSelected?.charAt(0).toUpperCase() === 'M' && filters.itemIdSelected.length > 1 &&
+                    <>
+                      <div className="pl-7 h-[30px] flex items-end justify-between leading-8 w-full z-40 dark:border-[#353535] border-[#d4d4d4] border-b">{
+                        filters.itemIdSelected?.toUpperCase() === 'MU' ? (     
+                          <>                
                             <div className="flex gap-2 font-semibold z-50 transition-color delay-75 h-full items-center">
+                                  <Departamentos />                            
                             </div>
                             <div className="flex justify-end align-bottom pr-6 z-50 transition-color delay-75 h-full !pb-[2px]">
-                                <MenuFiltersMan />
+                                  <MenuFiltersUsr />                            
                             </div>
-                          </>                        
-                        )
-                      }
-                    </div>
-                    <div className="overflow-auto h-full relative pr-2 w-full" id="containerRef">                      
-                        <ListaRegMantenedores frmRecord={frmRecord}/>                      
-                    </div>
-                  </>
-              }{
-                filters.itemIdSelected?.charAt(0).toUpperCase() === 'R' && filters.itemIdSelected.length > 1 &&
-                  <ListaRegReportes frmReport={frmReport} />
-              }
-        </div>
+                          </> 
+                          ) : filters.itemIdSelected?.toUpperCase() === 'MC' ? (  
+                            <>                
+                              <div className="flex gap-2 font-semibold z-50 transition-color delay-75 h-full items-center">
+                                    <Regiones />                            
+                              </div>
+                              <div className="flex justify-end align-bottom pr-6 z-50 transition-color delay-75 h-full !pb-[2px]">
+                                    <MenuFiltersCom />                            
+                              </div>
+                            </> 
+                          ) : filters.itemIdSelected?.toUpperCase() === 'MI' ? (  
+                            <>                
+                              <div className="flex gap-2 font-semibold z-50 transition-color delay-75 h-full items-center">
+                                    <ListaDesplegable />                            
+                              </div>
+                              <div className="flex justify-end align-bottom pr-6 z-50 transition-color delay-75 h-full !pb-[2px]">
+                                    <MenuFiltersListaDes />                            
+                              </div>
+                            </> 
+                          ) : (
+                            <>
+                              <div className="flex gap-2 font-semibold z-50 transition-color delay-75 h-full items-center">
+                              </div>
+                              <div className="flex justify-end align-bottom pr-6 z-50 transition-color delay-75 h-full !pb-[2px]">
+                                  <MenuFiltersMan />
+                              </div>
+                            </>                        
+                          )
+                        }
+                      </div>
+                      <div className="overflow-auto h-full relative pr-2 w-full" id="containerRef">                      
+                          <ListaRegMantenedores frmRecord={frmRecord}/>                      
+                      </div>
+                    </>
+                }{
+                  filters.itemIdSelected?.charAt(0).toUpperCase() === 'R' && filters.itemIdSelected.length > 1 &&
+                    <ListaRegReportes frmReport={frmReport} />
+                }
+          </div>
+      }
       </section>
     )
 }
--- src/components/main/DetalleRequerimiento.jsx
+++ src/components/main/DetalleRequerimiento.jsx
@@ -58,60 +58,62 @@ export default function DetalleRequerimiento(){
     const { request } = useRequest()
     const { filters } = useFilters()
     return(
-        <div className='sticky top-0 z-50'>
-        {filters.filterSearchResult && 
-            <h2 className='pl-7 text-lg py-2 border-t dark:!text-stone-100 !text-stone-500 dark:!border-[#353535] !border-[#d4d4d4] dark:!bg-[#262626] !bg-[#ffffff]'>Resultados</h2>
-        }
-            <Accordion className="z-0 dark:!bg-[#262626] !bg-[#ffffff]" slotProps={{ transition: { timeout: 350 } }}>
-                <AccordionSummary                 
-                    className='dark:!text-stone-100 !text-stone-500 dark:!border-[#353535] !border-[#d4d4d4] !text-[.7rem] !font-bold truncate dark:!bg-[#444444] !bg-[#f0f0f0] !py-1 hover:!dark:bg-[#666666] hover:!bg-[#e6f2fa] overflow-hidden !px-2 !h-7 !min-h-7 !m-0 !border-b dark:hover:!bg-[#505050]'
-                    >Detalle
-                </AccordionSummary>
-                <AccordionDetails className="py-0">
-                    <article className="p-3">
-                        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2">
-                            {request === null ? 
-                                <p className="mb-3 text-stone-500 dark:text-stone-400">Debes seleccionar un requerimiento para poder ver sus detalles</p> :
-                                <>                
-                                    <div className="mb-3 text-stone-500 dark:text-stone-400">
-                                        <ul className="space-y-2">
-                                            <li>Flujo: <span className='font-medium dark:text-stone-300'>{request?.request?.FLU_Descripcion} V.{request?.request?.VFL_Id}</span></li>
-                                            <li>Número: <span className='font-medium dark:text-stone-300'>{request?.request?.VRE_Id}</span></li>
-                                            <li>Creador: <span className='font-medium dark:text-stone-300'>{request?.request?.NombreCreador + ' ' + request?.request?.ApellidoCreador}</span></li>
-                                            <li>Dep. creación: <span className='font-medium dark:text-stone-300'>{request?.request?.DepDescripcionOrigen}</span></li>
-                                            <li>Creación: <span className='font-medium dark:text-stone-300'>{request?.request?.REQ_FechaEdit?.slice(8,10) + ' de ' + meses[parseInt(request?.request?.REQ_FechaEdit?.slice(5,7))] + ' de ' + request?.request?.REQ_FechaEdit?.slice(0,4)}</span></li>
-                                            <li>Modificacón: <span className='font-medium dark:text-stone-300'>{request?.request?.DRE_FechaEdit?.slice(8,10) + ' de ' + meses[parseInt(request?.request?.DRE_FechaEdit?.slice(5,7))] + ' de ' + request?.request?.DRE_FechaEdit?.slice(0,4)}</span></li>{
-                                            request?.request?.ESRAnterior_Descripcion ?                                            
-                                                <li>Acción realizada: <span className='font-medium dark:text-stone-300'>{parseInt(request?.request?.ESRAnterior_Descripcion)>=0 ? 'Creación' : request?.request?.ESRAnterior_Descripcion}</span></li>
-                                            : null}
-                                        </ul>
-                                    </div>
-                                    <div className="mb-3 text-stone-500 dark:text-stone-400">
-                                        <ul className="space-y-2">{
-                                            request?.request?.FLD_DiasLimites ?
-                                                <li>Días limite: <span className='font-medium dark:text-stone-300'>{request?.request?.FLD_DiasLimites}</span></li>
-                                            : null}{
-                                            request?.request?.DRE_DifDias ?                                             
-                                                <li>Dias sin responder: <span className='font-medium dark:text-stone-300'>{request?.request?.DRE_DifDias}</span></li>
-                                            : null}{
-                                            request?.request?.VFO_Id ?                                            
-                                                <li>Número del formulario: <span className='font-medium dark:text-stone-300'>{request?.request?.VFO_Id}</span></li>
-                                            : null}
-                                            <li>Paso actual: <span className='font-medium dark:text-stone-300'>{request?.request?.FLD_CodigoPaso}</span></li>
-                                            <li>Editor actual: <span className='font-medium dark:text-stone-300'>{request?.request?.NombreEditor ? request?.request?.NombreEditor + ' ' + request?.request?.ApellidoEditor : 'Esperando a ser tomado'}</span></li>                            
-                                            <li>Dep. actual: <span className='font-medium dark:text-stone-300'>{request?.request?.DepDescripcionActual}</span></li>{
-                                            request?.request?.ESR_AccionFlujoDatos ?                                            
-                                                <li>Acción a realizar: <span className='font-medium dark:text-stone-300'>{request?.request?.ESR_AccionFlujoDatos}</span></li>
-                                            : null}
-                                            <li>Estado del Req.: <span className='font-medium dark:text-stone-300'>{request?.request?.ESR_DescripcionRequerimiento ? request?.request?.ESR_DescripcionRequerimiento : 'Pendiente'}</span></li>                                            
-                                        </ul>
-                                    </div>
-                                </>
-                            }
-                        </div>
-                    </article> 
-                </AccordionDetails>
-            </Accordion>         
-        </div>
+        <>{
+            filters.filterSearchResult && 
+                <h2 className='pl-7 text-lg py-2 border-t dark:!text-stone-100 !text-stone-500 dark:!border-[#353535] !border-[#d4d4d4] dark:!bg-[#262626] !bg-[#ffffff]'>Resultados</h2>
+            }
+            <div className='sticky top-0 z-50'>
+                <Accordion className="z-0 dark:!bg-[#262626] !bg-[#ffffff]" slotProps={{ transition: { timeout: 350 } }}>
+                    <AccordionSummary                 
+                        className='dark:!text-stone-100 !text-stone-500 dark:!border-[#353535] !border-[#d4d4d4] !text-[.7rem] !font-bold truncate dark:!bg-[#444444] !bg-[#f0f0f0] !py-1 hover:!dark:bg-[#666666] hover:!bg-[#e6f2fa] overflow-hidden !px-2 !h-7 !min-h-7 !m-0 !border-b dark:hover:!bg-[#505050]'
+                        >Detalle
+                    </AccordionSummary>
+                    <AccordionDetails className="py-0">
+                        <article className="p-3">
+                            <div className="grid grid-cols-1 gap-6 sm:grid-cols-2">
+                                {request === null ? 
+                                    <p className="mb-3 text-stone-500 dark:text-stone-400">Debes seleccionar un requerimiento para poder ver sus detalles</p> :
+                                    <>                
+                                        <div className="mb-3 text-stone-500 dark:text-stone-400">
+                                            <ul className="space-y-2">
+                                                <li>Flujo: <span className='font-medium dark:text-stone-300'>{request?.request?.FLU_Descripcion} V.{request?.request?.VFL_Id}</span></li>
+                                                <li>Número: <span className='font-medium dark:text-stone-300'>{request?.request?.VRE_Id}</span></li>
+                                                <li>Creador: <span className='font-medium dark:text-stone-300'>{request?.request?.NombreCreador + ' ' + request?.request?.ApellidoCreador}</span></li>
+                                                <li>Dep. creación: <span className='font-medium dark:text-stone-300'>{request?.request?.DepDescripcionOrigen}</span></li>
+                                                <li>Creación: <span className='font-medium dark:text-stone-300'>{request?.request?.REQ_FechaEdit?.slice(8,10) + ' de ' + meses[parseInt(request?.request?.REQ_FechaEdit?.slice(5,7))] + ' de ' + request?.request?.REQ_FechaEdit?.slice(0,4)}</span></li>
+                                                <li>Modificacón: <span className='font-medium dark:text-stone-300'>{request?.request?.DRE_FechaEdit?.slice(8,10) + ' de ' + meses[parseInt(request?.request?.DRE_FechaEdit?.slice(5,7))] + ' de ' + request?.request?.DRE_FechaEdit?.slice(0,4)}</span></li>{
+                                                request?.request?.ESRAnterior_Descripcion ?                                            
+                                                    <li>Acción realizada: <span className='font-medium dark:text-stone-300'>{parseInt(request?.request?.ESRAnterior_Descripcion)>=0 ? 'Creación' : request?.request?.ESRAnterior_Descripcion}</span></li>
+                                                : null}
+                                            </ul>
+                                        </div>
+                                        <div className="mb-3 text-stone-500 dark:text-stone-400">
+                                            <ul className="space-y-2">{
+                                                request?.request?.FLD_DiasLimites ?
+                                                    <li>Días limite: <span className='font-medium dark:text-stone-300'>{request?.request?.FLD_DiasLimites}</span></li>
+                                                : null}{
+                                                request?.request?.DRE_DifDias ?                                             
+                                                    <li>Dias sin responder: <span className='font-medium dark:text-stone-300'>{request?.request?.DRE_DifDias}</span></li>
+                                                : null}{
+                                                request?.request?.VFO_Id ?                                            
+                                                    <li>Número del formulario: <span className='font-medium dark:text-stone-300'>{request?.request?.VFO_Id}</span></li>
+                                                : null}
+                                                <li>Paso actual: <span className='font-medium dark:text-stone-300'>{request?.request?.FLD_CodigoPaso}</span></li>
+                                                <li>Editor actual: <span className='font-medium dark:text-stone-300'>{request?.request?.NombreEditor ? request?.request?.NombreEditor + ' ' + request?.request?.ApellidoEditor : 'Esperando a ser tomado'}</span></li>                            
+                                                <li>Dep. actual: <span className='font-medium dark:text-stone-300'>{request?.request?.DepDescripcionActual}</span></li>{
+                                                request?.request?.ESR_AccionFlujoDatos ?                                            
+                                                    <li>Acción a realizar: <span className='font-medium dark:text-stone-300'>{request?.request?.ESR_AccionFlujoDatos}</span></li>
+                                                : null}
+                                                <li>Estado del Req.: <span className='font-medium dark:text-stone-300'>{request?.request?.ESR_DescripcionRequerimiento ? request?.request?.ESR_DescripcionRequerimiento : 'Pendiente'}</span></li>                                            
+                                            </ul>
+                                        </div>
+                                    </>
+                                }
+                            </div>
+                        </article> 
+                    </AccordionDetails>
+                </Accordion>         
+            </div>
+        </>
     )
 }
